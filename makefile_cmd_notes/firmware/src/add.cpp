513dd5fcd47a7ac2c946abea24c8aaec53f40fcb
lisilin013 <lisilin013@163.com>
2017-07-13 18:13:24 +0800

    del makefile_cmd

--- makefile_cmd_notes/firmware/src/add.cpp
+++
@@ -1,8 +0,0 @@
-#include "add.h"
-#include <iostream>
-
-float get_sqrt(float data)
-{
-        std::cout<<"I am get_sqrt\n";
-        return sqrt(data);
-}
--- makefile_cmd_notes/firmware/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "add.h"
-#include <iostream>
-
-int main(int agrc, char* agrv)
-{
-    std::cout<<"main func"<<get_sqrt(4.0)<<std::endl;
-}
