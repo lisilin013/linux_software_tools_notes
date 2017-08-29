e366dbc6289f84ecd2b81251195e40c1e7833837
lisilin013 <lisilin013@163.com>
2017-08-29 11:07:21 +0800

    sublime highlight sh script

---
+++ sublime_hotkey_notes/sublime_highlight.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+cd ~/.config/sublime-text-3/Packages
+
+# cmake highlight
+https://github.com/zyxar/Sublime-CMakeLists.git
+
+# markdown highlight
+https://github.com/SublimeText-Markdown/MarkdownEditing.git
